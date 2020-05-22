e24ab4cc4f447f9b8aed43263f338aab784c6961
jia.jiwei <jia.jiwei@sfit.shfe.com.cn>
2020-05-22 13:06:26 +0800

    auto commit

--- bin/autocommit.sh
+++ bin/autocommit.sh
@@ -1,3 +1,6 @@
 #!/bin/bash 
 cd /home/jiajw/source/jdk-jdk-14-22/note
-git status -s | awk  '{if($1 =="??") print $2}'
+git status -s | awk  '{if($1 =="??")  print  $2}' | xargs  -n1 git add
+git commit  -m"auto commit"
+git push 
+echo $?
